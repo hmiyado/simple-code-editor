0e2c21a2c4ebabe496f9a368a67385a1294f60f9
miyado <10195648+hmiyado@users.noreply.github.com>
2025-07-05 11:53:02 +0900

    Refactor Language type definition to simplify structure and improve type inference

--- src/editor/Language.ts
+++ src/editor/Language.ts
@@ -1,35 +1,4 @@
-export type Language =
-  | {
-      // The label is the name of the language, and the value is the identifier for Monaco editor.
-      label: "JavaScript";
-      value: "javascript";
-    }
-  | {
-      label: "TypeScript";
-      value: "typescript";
-    }
-  | {
-      label: "Python";
-      value: "python";
-    }
-  | {
-      label: "Kotlin";
-      value: "kotlin";
-    }
-  | {
-      label: "Swift";
-      value: "swift";
-    }
-  | {
-      label: "C++";
-      value: "cpp";
-    }
-  | {
-      label: "Java";
-      value: "java";
-    };
-
-export const Languages: Language[] = [
+export const Languages = [
   { label: "C++", value: "cpp" },
   { label: "Java", value: "java" },
   { label: "JavaScript", value: "javascript" },
@@ -37,7 +6,9 @@ export const Languages: Language[] = [
   { label: "Python", value: "python" },
   { label: "Swift", value: "swift" },
   { label: "TypeScript", value: "typescript" },
-];
+] as const;
+
+export type Language = (typeof Languages)[number];
 
 export const getLanguage = (lang: string): Language => {
   return Languages.find((language) => language.value === lang) ?? Languages[0];
